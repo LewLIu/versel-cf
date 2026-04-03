91d26790857dfefdff06f069acc558395e38b841
LewLIu <qpdqpd@qq.com>
2026-04-03 17:43:53 +0800

    Update [...path].js

--- api/[...path].js
+++ api/[...path].js
@@ -6,14 +6,12 @@ export default async function handler(req) {
   const TARGET_WORKER = process.env.TARGET_WORKER
   const AUTH_KEY = process.env.AUTH_KEY
 
-  // 鉴权
   const url = new URL(req.url)
   const auth = url.searchParams.get('auth')
   if (AUTH_KEY && auth !== AUTH_KEY) {
     return new Response('Unauthorized', { status: 401 })
   }
 
-  // 直接把完整路径拼过去
   const targetUrl = TARGET_WORKER.replace(/\/$/, '') + url.pathname + url.search
 
   const res = await fetch(targetUrl, {
@@ -26,7 +24,8 @@ export default async function handler(req) {
     status: res.status,
     headers: {
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET,POST,OPTIONS'
+      'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+      'Content-Type': 'application/json; charset=utf-8'
     }
   })
 }
